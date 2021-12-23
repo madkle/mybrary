0ec276bc87ab51204e88d0bc3f3a3de1dbf382d8
madkle <62890073+madkle@users.noreply.github.com>
2021-12-23 17:34:22 +0100

    initial Author Routes

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ if(process.env.NODE_ENV !== 'production'){
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
-const indexRouter = require('./routes/index.js')
+const indexRouter = require('./routes/index.js');
+const authorRouter = require('./routes/authors.js');
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
@@ -13,7 +15,7 @@ app.set('layout', 'layouts/layout');
 
 app.use(expressLayouts);
 app.use(express.static('public'));
-
+app.use(bodyParser.urlencoded({limit: '10mb', extended:false }))
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});
 const db = mongoose.connection;
@@ -21,5 +23,6 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log('connected to Mongoose'))
 
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 app.listen(process.env.PORT || 3000);
